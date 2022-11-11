538d35f7d10b78c8435e6082bf28a8da8681662f
mar-cial <cesarmarcialq@gmail.com>
2022-11-11 13:41:24 -0800

    add page counter

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+      <>
+      <Script src="https://counter4.optistats.ovh/private/counter.js?c=81mp5d88yqtw1ac22kc5t8a87nyjmwyh&down=async" async/>
+      
       <Component {...pageProps} />
+      </>
   );
 }
 
